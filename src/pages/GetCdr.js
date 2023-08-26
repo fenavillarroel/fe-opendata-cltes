7ca5b0f9f958744a998a0b59fd4315d256b6728a
Fernando Villarroel <fenavillarroel@gmail.com>
2023-08-26 18:35:15 -0400

    Render cdr and navigation

--- src/pages/GetCdr.js
+++ src/pages/GetCdr.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-
 import jwt from 'jwt-decode'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const GetCdr = ( {token} ) => {
 
     let fd = new Date();
     fd.setDate(fd.getDate());
     let date = fd.toISOString().substring(0,10);
- 
+     
     const user = jwt(token);
 
     const [fromDate, setFromDate] = useState(date);
@@ -107,21 +107,48 @@ const GetCdr = ( {token} ) => {
                             <table>
                                 <thead>
                                     <tr>
-                                        <th>ID</th>
-                                        <th>Name</th>
-                                        <th>Date</th>
+                                        <th>Ani</th>
+                                        <th>Destino</th>
+                                        <th>Inicio</th>
+                                        <th>Answer</th>
+                                        <th>Fin</th>
+                                        <th>Billsec</th>
+                                        <th>Disposición</th>
+                                        <th>Descripción</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {dataResult.map((item, index) => (
                                         <tr key={index}>
-                                            <td>{item.id}</td>
-                                            <td>{item.name}</td>
-                                            <td>{item.date}</td>
+                                            <td>{item.ani}</td>
+                                            <td>{item.destino}</td>
+                                            <td>{item.inicio}</td>
+                                            <td>{item.answer}</td>
+                                            <td>{item.fin}</td>
+                                            <td>{item.billsec}</td>
+                                            <td>{item.disposicion}</td>
+                                            <td>{item.descripcion}</td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
+                            <nav aria-label="Page navigation">
+                                <ul className="pagination">
+                                {Array.from({ length: totalPages }, (_, index) => (
+                                    <li
+                                    key={index}
+                                    className={`page-item ${index + 1 === currentPage ? 'active' : ''}`}
+                                    >
+                                    <button
+                                        className="page-link"
+                                        onClick={() => handlePageChange(index + 1)}
+                                    >
+                                        {index + 1}
+                                    </button>
+                                    </li>
+                                ))}
+                                </ul>
+                            </nav>
                         </div>
                         </div>
                         <div className="col-2">
